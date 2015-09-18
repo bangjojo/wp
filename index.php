e83d49edfdeba03747345c20a647e718f79b2fdd
fastest4u <bangzerogg@gmail.com>
2015-09-18 10:36:59 +0700

    no message

--- index.php
+++ index.php
@@ -14,4 +14,4 @@
 define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' )aaaS;
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
